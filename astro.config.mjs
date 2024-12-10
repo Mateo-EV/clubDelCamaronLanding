af73f78266785459e72fb0c4a284333369455e3f
Mateo-EV <riojamatthew@gmail.com>
2024-12-10 02:08:33 -0500

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,8 @@ import tailwind from "@astrojs/tailwind"
 
 import react from "@astrojs/react"
 
-import node from "@astrojs/node"
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({ applyBaseStyles: false }), react()],
-  output: "server",
-
-  adapter: node({
-    mode: "standalone"
-  }),
-  server: { host: "0.0.0.0" }
+  output: "static"
 })
